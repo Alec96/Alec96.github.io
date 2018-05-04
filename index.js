6f9ba48cdf2795dd1995c84b7071aa6cd33abd84
Alec Li <li.ale@husky.neu.edu>
2018-05-03 20:14:31 -0400

    made changing pictures dynamic

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
 
 	var rottenTomatillos = {
 		type: "Class Project",
-		description: "This is the Rotten Tomatillos Description"
+		description: "This is the Rotten Tomatillos Description",
+		img: "rottentomatillos.jpg"
 	}
 	var musicEditor = {
 		type: "Class Project",
-		description: "This is the Music Editor Description"
+		description: "This is the Music Editor Description",
+		img: "me.jpg"
 	}
 
 	var projects = {"Rotten Tomatillos": rottenTomatillos, "Music Editor": musicEditor}
@@ -15,5 +17,6 @@ $(document).ready(function(){
         $("#cardTitle").html(this.value);
         $("#cardSubtitle").html(projects[this.value].type);
         $("#cardBody").html(projects[this.value].description);
+        $("#projectImage").attr("src", projects[this.value].img);
     });
 });
